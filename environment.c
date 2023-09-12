b3ccf6cd041161f0fd275b34e3104ca8c5893039
kareem121004 <karimadel11112004@gmail.com>
2023-09-12 07:19:04 -0700

    env upload

---
+++ environment.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * _getenv - retrieves the value of an environment
+ * @name: the name of the environment variable to retrieve
+ * Return: the value of the environment variable
+*/
+
+char *_getenv(const char *name)
+{
+	extern char **environ;
+	int i = 0;
+	char *token;
+
+	while (environ[i])
+	{
+		token = strtok(environ[i], "=");
+
+		if (strcmp(name, token) == 0)
+			return (strtok(NULL, "\n");
+		i++;
+	}
+	return (NULL);
+}
