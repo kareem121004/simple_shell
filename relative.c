7ec19ef8e071d1ae92b798d4bfbf52229a631f13
kareem121004 <karimadel11112004@gmail.com>
2023-09-20 16:04:14 -0700

    edit again

--- relative.c
+++ relative.c
@@ -52,11 +52,12 @@ void shell_exit(char **comm)
 {
 	int exit_status = 0;
 
-	if (comm[1] != NULL)
+	if (comm[1] == NULL)
 	{
-		exit_status = _atoi(comm[1]);
-
+		free_dp(comm);
+		exit(EXIT_SUCCESS);
 	}
+	exit_status = _atoi(comm[1]);
 	free_dp(comm);
 	exit(EXIT_SUCCESS);
 }
